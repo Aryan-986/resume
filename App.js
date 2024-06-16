88fdd7c7ae40a30eff639ed0bff9c0a64ac96f5b
Aryan-986 <aryankarki984@gmail.com>
2024-06-16 23:29:07 +0545

    added index.html

---
+++ App.js
@@ -0,0 +1,21 @@
+//src/App.js
+import React from 'react';
+import ProductDetail from './ProductDetail';
+import './style.css';
+
+//Define product data
+const products = [
+    {
+        id: 1,
+        name: 'Nikko Attar',
+        description: 'Niko Attar is a fregrance by the luxury perfume brand,Al Naseem. The fragrance is often described as a blend of exotic and alluring scents, capturing the essence of the Middle East with its rich and distinctive aroma. It typically features notes such as oud, amber, musk, and floral undertones, creating a captivating and long-lasting scent experience. Al Naseem Niko Attar is known for its sophistication and elegance, making it a popular choice for those who appreciate high-quality fragrances. This exquisite fragrance seamlessly weaves together the deep, aromatic notes of oud with the soft allure of floral undertones, creating a captivating aroma that lingers beautifully. Whether worn for a special occasion or as an everyday signature, Niko Attar embodies elegance and grace, making every moment unforgettable.',
+        price: 999,
+        img: 'niko.jpg',
+
+
+
+
+
+                          
+    }
+]
